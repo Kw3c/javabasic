947e676c19ed0beb7828fb012a4610c76959e2b1
Kw3c <shuttlecock@empas.com>
2022-11-13 16:34:15 +0900

    소소한 수정

--- src/main/java/org/example/inherit/IdolDancer.java
+++ src/main/java/org/example/inherit/IdolDancer.java
@@ -1,7 +1,14 @@
 package org.example.inherit;
 
 import org.example.encap.Dancer;
+import org.example.encap.Grade;
 
 public class IdolDancer extends Dancer {
+    public IdolDancer(String crewName, Grade grade, String myName) {
+        super(crewName,grade,myName);
+    }
 
+    public void dance() {
+        System.out.println("dance with winks.");
+    }
 }
--- src/main/java/org/example/inherit/StreetDancer.java
+++ src/main/java/org/example/inherit/StreetDancer.java
@@ -1,6 +1,14 @@
 package org.example.inherit;
 
 import org.example.encap.Dancer;
+import org.example.encap.Grade;
 
 public class StreetDancer extends Dancer {
+    public StreetDancer(String crewName, Grade grade, String myName) {
+        super(crewName,grade,myName);
+    }
+
+    public void dance() {
+        System.out.println("dance on street.");
+    }
 }
